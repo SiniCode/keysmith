685d12b7c1d260175a2dd0afe755e036a816b990
Sini Arkko <sini.arkko@gmail.com>
2022-03-26 21:43:10 +0200

    Add primality test

--- src/keys.py
+++ src/keys.py
@@ -1,5 +1,14 @@
 import random
 
+def find_exponent(number):
+    result = (1, 1)
+    power = 1
+    while True:
+        if number % (2 ** power) != 0:
+            break
+        result = (number // (2 ** power), power)
+        power += 1
+    return result
 
 def miller_rabin_primality_test(number, iterations):
     """This function is a probabilistic primality test for the given number.
@@ -14,6 +23,22 @@ def miller_rabin_primality_test(number, iterations):
         False if it is not.
     """
 
+    exponent, power = find_exponent(number-1)
+
+    for i in range(iterations):
+        base = random.randint(2, number-2)
+        test_value = pow(base, exponent, number)
+        if test_value != 1 and test_value != number-1:
+            j = 1
+            while j < power and test_value != number-1:
+                test_value = pow(test_value, 2, number)
+                if test_value == 1:
+                    return False
+                j += 1
+            if test_value != number-1:
+                return False
+
+    return True
 
 
 def create_keys(length):
@@ -29,6 +54,9 @@ def create_keys(length):
     primes = []
     while len(primes) < 2:
         number = random.getrandbits(length)
+        if number == 2 or number == 3:
+            primes.append(number)
+            continue
         if number == 1 or number % 2 == 0:
             continue
         if miller_rabin_primality_test(number) == False:
